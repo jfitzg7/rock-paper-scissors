be6f63204050b7fb7689dc063f927ef1cf98c4e0
jfitzg7 <jackf8088@gmail.com>
2022-06-29 09:13:00 -0600

    Implemented playRound function

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -4,3 +4,48 @@ function computerPlay() {
   // generate a random integer between 0 and 2 and use that as the index
   return choices[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    typeof playerSelection === "string" ||
+    playerSelection instanceof String
+  ) {
+    playerSelection = playerSelection.toLowerCase();
+  }
+  if (
+    typeof computerSelection === "string" ||
+    computerSelection instanceof String
+  ) {
+    computerSelection = computerSelection.toLowerCase();
+  }
+
+  let decisionStr = null;
+
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      decisionStr = "Tie game! Rock ties Rock.";
+    } else if (computerSelection === "paper") {
+      decisionStr = "You lose! Paper beats Rock.";
+    } else if (computerSelection === "scissors") {
+      decisionStr = "You win! Rock beats Scissors.";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      decisionstr = "You win! Paper beats Rock.";
+    } else if (computerSelection === "paper") {
+      decisionStr = "Tie game! Paper ties Paper.";
+    } else if (computerSelection === "scissors") {
+      decisionStr = "You lose! Scissors beats Paper.";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      decisionStr = "You lose! Rock beats Scissors.";
+    } else if (computerSelection === "paper") {
+      decisionStr = "You win! Scissors beats Paper.";
+    } else if (computerSelection === "scissors") {
+      decisionStr = "Tie game! Scissors ties Scissors.";
+    }
+  }
+
+  return decisionStr;
+}
