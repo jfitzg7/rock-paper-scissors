face4245a039323a3352cecce1f23c4296a7552e
jfitzg7 <jackf8088@gmail.com>
2022-07-02 07:29:34 -0600

    Got the general layout for the rps ui

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -19,7 +19,11 @@ function isWin(selection1, selection2) {
   return false;
 }
 
-// didThePlayerWin and didTheComputerWin help with overall refactoring, and tallying up total wins in the game() function
+/* 
+  didThePlayerWin and didTheComputerWin help with overall refactoring, 
+  and tallying up total wins in the game() function.
+*/
+
 function didThePlayerWin(playerSelection, computerSelection) {
   return isWin(playerSelection, computerSelection);
 }
@@ -28,71 +32,30 @@ function didTheComputerWin(playerSelection, computerSelection) {
   return isWin(computerSelection, playerSelection);
 }
 
-function playRound(playerSelection, computerSelection) {
-  let decisionStr = null;
+function generateDecisionString(playerSelection, computerSelection) {
+  let decisionString = null;
 
   playerWinState = didThePlayerWin(playerSelection, computerSelection);
   computerWinState = didTheComputerWin(playerSelection, computerSelection);
 
   if (playerWinState && !computerWinState) {
-    decisionStr = "You win! " + playerSelection + " beats " + computerSelection;
+    decisionString =
+      "You win! " + playerSelection + " beats " + computerSelection;
   } else if (!playerWinState && computerWinState) {
-    decisionStr =
+    decisionString =
       "You lose! " + computerSelection + " beats " + playerSelection;
   } else if (!playerWinState && !computerWinState) {
-    decisionStr =
+    decisionString =
       "Tie round! " + playerSelection + " ties " + computerSelection;
   }
 
-  return decisionStr;
+  return decisionString;
 }
 
-function game() {
-  let playerWinTotal = 0;
-  let computerWinTotal = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Type 'rock', 'paper', or 'scissors'");
-    let computerSelection = computerPlay();
-
-    // input sanitization is done here for the isWin function
-    if (
-      typeof playerSelection === "string" ||
-      playerSelection instanceof String
-    ) {
-      playerSelection = playerSelection.toLowerCase();
-    }
-
-    if (
-      typeof computerSelection === "string" ||
-      computerSelection instanceof String
-    ) {
-      computerSelection = computerSelection.toLowerCase();
-    }
-
-    const validChoices = ["rock", "paper", "scissors"];
-    if (
-      !validChoices.includes(playerSelection) ||
-      !validChoices.includes(computerSelection)
-    ) {
-      console.log(
-        "Error: playerSelection and/or computerSelection are not valid choices"
-      );
-    } else {
-      if (didThePlayerWin(playerSelection, computerSelection)) {
-        playerWinTotal++;
-      } else if (didTheComputerWin(playerSelection, computerSelection)) {
-        computerWinTotal++;
-      }
-      console.log(playRound(playerSelection, computerSelection));
-    }
-  }
-
-  if (playerWinTotal > computerWinTotal) {
-    console.log("You won the game!");
-  } else if (playerWinTotal < computerWinTotal) {
-    console.log("You lost the game!");
-  } else {
-    console.log("Tie game!");
-  }
-}
+const playerButtons = document.querySelectorAll(".player-btn");
+playerButtons.forEach((button) => {
+  console.log(button.id);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+  });
+});
